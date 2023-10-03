6df3a7daada3e91bb551ced64f85fe1bf1599649
Chris Harper <charper+git@charper.co.uk>
2023-10-03 10:33:43 +0100

    Remove LSP zero

--- home/charper/neovim.nix
+++ home/charper/neovim.nix
@@ -1,17 +1,5 @@
 { pkgs, ... }:
 
-let
-  #pull from default(2.x) branch instead of main(1.x) 28/08/2023
-  custom-lsp-zero-nvim = pkgs.vimUtils.buildVimPluginFrom2Nix {
-    name = "custom-lsp-zero-nvim";
-    src = pkgs.fetchFromGitHub {
-      owner = "VonHeikemen";
-      repo = "lsp-zero.nvim";
-      rev = "f084f4a6a716f55bf9c4026e73027bb24a0325a3";
-      sha256 = "hBDkb4KYkuhI7Vv5UUtdLUPechCt40UxUSRr3eZqHG8=";
-    };
-  };
-in
 {
 
   programs.neovim = {
@@ -28,14 +16,12 @@ in
 
 
       nvim-lspconfig
+      nvim-cmp
       cmp-nvim-lsp
       cmp-buffer
       cmp-path
-      cmp-cmdline
-      nvim-cmp
       luasnip
 
-      custom-lsp-zero-nvim
 
     ];
     extraLuaConfig = ''
@@ -74,21 +60,67 @@ in
       vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
+      -- Add additional capabilities supported by nvim-cmp
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lsp = require('lsp-zero').preset({})
+      local lspconfig = require('lspconfig')
 
-      lsp.on_attach(function(client, bufnr)
-        -- see :help lsp-zero-keybindings
-        -- to learn the available actions
-        lsp.default_keymaps({buffer = bufnr})
-      end)
+      -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+      local servers = { 'bashls','nil_ls'}
+      for _, lsp in ipairs(servers) do
+        lspconfig[lsp].setup {
+          -- on_attach = my_custom_on_attach,
+          capabilities = capabilities,
+        }
+      end
 
-      -- When you don't have mason.nvim installed
-      -- You'll need to list the servers installed in your system
-      lsp.setup_servers({'bashls','nil_ls'})
-      require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+      -- luasnip setup
+      local luasnip = require 'luasnip'
 
-      lsp.setup()
+      -- nvim-cmp setup
+      local cmp = require 'cmp'
+      cmp.setup {
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+          ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+          -- C-b (back) C-f (forward) for snippet placeholder navigation.
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          },
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+        }),
+        sources = {
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'buffer' },
+          { name = 'path' },
+        },
+      } 
+
     '';
   };
 
