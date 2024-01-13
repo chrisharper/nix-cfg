4e65db3c6a7ee4ad6648005b40d775328fdd59a9
Chris Harper <charper+git@charper.co.uk>
2024-01-13 20:26:49 +0000

    Change redundant tmux settings

--- home/charper/tmux.nix
+++ home/charper/tmux.nix
@@ -30,9 +30,6 @@
       unbind C-Right
 
 
-      bind c new-window -c '#{pane_current_path}'
-      bind '"' split-window -c '#{pane_current_path}'
-      bind % split-window -h -c '#{pane_current_path}'
     '';
   };
 }
